6ee5c70af9cdba34814f5de2224e71e721da6c8e
Belma Gutlic <belma.dzi@gmail.com>
2020-01-13 18:25:28 +0100

    Address PR comment

--- src/keycache.ts
+++ src/keycache.ts
@@ -1,4 +1,3 @@
-import {Mutex} from 'async-mutex';
 import {PeerId} from "./@types/libp2p";
 import {bytes, bytes32} from "./@types/basic";
 
@@ -6,32 +5,14 @@ import {bytes, bytes32} from "./@types/basic";
  * Storage for static keys of previously connected peers.
  */
 class Keycache {
-  private mutex: Mutex;
   private storage = new Map<bytes, bytes32>();
 
-  constructor() {
-    this.mutex = new Mutex();
-  }
-
   public async store(peerId: PeerId, key: bytes32): Promise<void> {
-    const release = await this.mutex.acquire();
-    try {
-      this.storage.set(peerId.id, key);
-    } finally {
-      release();
-    }
+    this.storage.set(peerId.id, key);
   }
 
   public async load(peerId: PeerId): Promise<bytes32|null> {
-    const release = await this.mutex.acquire();
-    let key;
-    try {
-      key = this.storage.get(peerId.id) || null;
-    } finally {
-      release();
-    }
-
-    return key;
+    return this.storage.get(peerId.id) || null;
   }
 
   public resetStorage(): void {
