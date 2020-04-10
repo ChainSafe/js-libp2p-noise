493d2901a928d71d63e5c5fee817c4e30b2a45bf
Matija Petrunic <matija.petrunic@gmail.com>
2020-04-10 14:54:50 +0200

    Set dump session keys to false

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 export const NOISE_MSG_MAX_LENGTH_BYTES = 65535;
 export const NOISE_MSG_MAX_LENGTH_BYTES_WITHOUT_TAG = NOISE_MSG_MAX_LENGTH_BYTES - 16;
 
-export const DUMP_SESSION_KEYS = true;
+export const DUMP_SESSION_KEYS = false;
 
 
