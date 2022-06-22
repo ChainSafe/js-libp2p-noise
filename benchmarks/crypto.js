347844a66006698e42e590fddbafdf1ea8a2d3e0
Tuyen Nguyen <vutuyen2636@gmail.com>
2022-06-22 09:33:37 +0700

    chore: benchmark different Buffer size

--- benchmarks/crypto.js
+++ benchmarks/crypto.js
@@ -4,41 +4,47 @@ import {sodiumNative} from '../dist/src/crypto/sodium-native.js'
 import benchmark from 'benchmark'
 
 const main = async function () {
-  const data = Buffer.from('encryptthis encryptthis encryptthis encryptthis')
   const nonce = 1000
   const nonceBytes = new Uint8Array(12)
   new DataView(nonceBytes.buffer, nonceBytes.byteOffset, nonceBytes.byteLength).setUint32(4, nonce, true)
-  const key =  new Uint8Array(Array.from({length: 32}, () => 1))
-  const encrypted = stablelib.chaCha20Poly1305Encrypt(data, nonceBytes, new Uint8Array(0), key)
+  const key = new Uint8Array(Array.from({length: 32}, () => 1))
 
-  for(const {id, crypto} of [
-    {id: 'stablelib decrypt', crypto: stablelib},
-    {id: 'sodium-native decrypt', crypto: sodiumNative}
-  ]) {
-    const bench = new benchmark(id, {
-      fn: async function () {
-        crypto.chaCha20Poly1305Decrypt(encrypted, nonceBytes, new Uint8Array(0), key)
-      }
-    })
-    .on('complete', function (stats) {
-      console.log(String(stats.currentTarget))
-    })
-    bench.run()
-  }
+  // NOISE_MSG_MAX_LENGTH_BYTES = 65535
+  for (const length of [100, 500, 500, 5000, 20000, 65535]) {
+    const data100 = Buffer.from('encryptthis encryptthis encryptthis encryptthis encryptthis encryptthis encryptthis encryptthis encr')
+    const data = Buffer.concat(Array.from({length: Math.floor(length / 100)}, () => data100))
 
-  for(const {id, crypto} of [
-    {id: 'stablelib encrypt', crypto: stablelib},
-    {id: 'sodium-native encrypt', crypto: sodiumNative}
-  ]) {
-    const bench = new benchmark(id, {
-      fn: async function () {
-        crypto.chaCha20Poly1305Encrypt(data, nonceBytes, new Uint8Array(0), key)
-      }
-    })
-    .on('complete', function (stats) {
-      console.log(String(stats.currentTarget))
-    })
-    bench.run()
+    const encrypted = stablelib.chaCha20Poly1305Encrypt(data, nonceBytes, new Uint8Array(0), key)
+
+    for(const {id, crypto} of [
+      {id: `stablelib decrypt ${length} bytes Buffer`, crypto: stablelib},
+      {id: `sodium-native decrypt ${length} bytes Buffer`, crypto: sodiumNative}
+    ]) {
+      const bench = new benchmark(id, {
+        fn: async function () {
+          crypto.chaCha20Poly1305Decrypt(encrypted, nonceBytes, new Uint8Array(0), key)
+        }
+      })
+      .on('complete', function (stats) {
+        console.log(String(stats.currentTarget))
+      })
+      bench.run()
+    }
+
+    for(const {id, crypto} of [
+      {id: `stablelib encrypt ${length} bytes Buffer`, crypto: stablelib},
+      {id: `sodium-native encrypt ${length} bytes Buffer`, crypto: sodiumNative}
+    ]) {
+      const bench = new benchmark(id, {
+        fn: async function () {
+          crypto.chaCha20Poly1305Encrypt(data, nonceBytes, new Uint8Array(0), key)
+        }
+      })
+      .on('complete', function (stats) {
+        console.log(String(stats.currentTarget))
+      })
+      bench.run()
+    }
   }
 }
 
