13584098db2b5021bb5b979881f5fdf98ddf1100
Alan Shaw <alan.shaw@protocol.ai>
2022-08-10 14:45:26 +0100

    feat!: remove default instance (#188)
    * refactor: remove default instance
    
    * docs: update example to remove default instance usage

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,3 @@
-import { Noise } from './noise.js'
-
 export * from './crypto.js'
 export * from './crypto/stablelib.js'
 export * from './noise.js'
-
-/**
- * Default configuration, it will generate new noise static key and enable noise pipes (IK handshake).
- */
-export const NOISE = new Noise()
