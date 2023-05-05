3d1c98520c191637db836df18e3ebdcf9a92e43c
Marin Petrunic <marin.petrunic@gmail.com>
2023-05-05 12:01:43 +0200

    fix lint
    Signed-off-by: Marin Petrunic <marin.petrunic@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import type { ConnectionEncrypter } from '@libp2p/interface-connection-encrypter
 import { Noise } from './noise.js'
 import type { NoiseInit } from './noise.js'
 import type { NoiseExtensions } from './proto/payload.js'
-export { ICryptoInterface } from './crypto.js'
+export type { ICryptoInterface } from './crypto.js'
 export { pureJsCrypto } from './crypto/js.js'
 
 export function noise (init: NoiseInit = {}): () => ConnectionEncrypter<NoiseExtensions> {
